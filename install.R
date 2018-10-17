4f2d270c82843fe9b465dbd1dda84cd3a49dff47
Ming Tang <mingtang@huitadmins-MacBook-Pro.local>
2018-10-17 14:56:20 -0400

    install seurat

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 install.packages("tidyverse")
 install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages('leaflet')
+install.packages('Seurat')
+source("https://bioconductor.org/biocLite.R")
+biocLite("SIMLR")
